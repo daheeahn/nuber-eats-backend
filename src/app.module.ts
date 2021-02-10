7bc1b3755c3295f66d4820dd76bbc956984e48af
daheeahn <deg9810@gmail.com>
2021-02-10 21:17:02 +0900

    # 2.5 Configuring ConfigService (06:12)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,21 +4,27 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: process.env.NODE_DEV === 'dev' ? '.env.dev' : '.env.test', // prod은 나중에
+      ignoreEnvFile: process.env.NODE_ENV === 'prod', // production 환경일 땐 환경변수 파일 사용 X 다른 방법으로 얻는다.
+      // validationSchema // 환경변수의 유효성 검증 (다음 강의)
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5000,
-      username: 'daheeahn',
-      // localhost로 연결되어 있다면 비밀번호 묻지 않는다.
-      password: '12345', // 그러나 릴리즈에서는 제대로된 비밀번호를 써야겠지.
-      database: 'nuber-eats',
+      // 안먹힌다!!!!!!
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      // host: 'localhost',
+      // port: 5000,
+      // username: 'daheeahn',
+      // password: '12345',
+      // database: 'nuber-eats',
       synchronize: true, // 데이터베이스를 내 모듈의 현재 상태로 마이그레이션한다는 뜻
       logging: true, // 데이터베이스에서 무슨 일이 일어나는지 콘솔에 표시
     }),
