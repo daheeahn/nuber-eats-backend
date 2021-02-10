fd62c173605a059c5cffae73df5cc3450418fe8d
daheeahn <deg9810@gmail.com>
2021-02-10 13:40:35 +0900

    # 1.3 Arguments (03:56)

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,9 +1,8 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Args } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 // import { Query } from '@nestjs/common'; // 이거 아님!!!!!!!!!!!!1
 
-// @Resolver((of) => Restaurant) // 꼭 이렇게 안해도 됨. 그냥 이름 붙여주는거인듯?
-@Resolver()
+@Resolver((of) => Restaurant) // 꼭 이렇게 안해도 됨. 그냥 이름 붙여주는거. 직관성 높아짐.
 export class RestaurantResolver {
   // Query는 1번째 argument로 'function'이 필요하다. () => Boolean = reutnrs => Boolean
   @Query((returns) => Boolean) // => Boolean is for graphql (required)
@@ -12,8 +11,9 @@ export class RestaurantResolver {
     return true;
   }
 
-  @Query((returns) => Restaurant)
-  myRestaurant() {
-    return true;
+  @Query((returns) => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    console.log('veganOnly', veganOnly);
+    return [];
   }
 }
