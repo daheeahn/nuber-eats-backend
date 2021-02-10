9b200c7e27dee15d739f037240b606327c750ca5
daheeahn <deg9810@gmail.com>
2021-02-10 14:22:24 +0900

    # 1.5 Validating ArgsTypes (04:28)

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,16 +1,25 @@
 import { InputType, Field, ArgsType } from '@nestjs/graphql';
 import { Restaurant } from '../entities/restaurant.entity';
+import { IsString, IsBoolean, Length } from 'class-validator';
 
 // InputType is One Object
 // ArgsType is 분리된 값들을 args로 넘길 수 있게 해줌. => object로 안보내도 됨
 @ArgsType()
 export class CreateRestaurantDto {
   @Field((type) => String)
+  @IsString()
+  @Length(5, 10)
   name: string;
+
   @Field((type) => Boolean)
+  @IsBoolean()
   isVegan: boolean;
+
   @Field((type) => String)
+  @IsString()
   address: string;
+
   @Field((type) => String)
+  @IsString()
   ownerName: string;
 }
