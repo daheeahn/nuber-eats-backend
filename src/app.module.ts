2f59aef2be4d462a663c5b9efce75ddbe7c5b88b
daheeahn <deg9810@gmail.com>
2021-02-10 21:18:25 +0900

    에러 해결 (오타)

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,23 +8,17 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_DEV === 'dev' ? '.env.dev' : '.env.test', // prod은 나중에
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test', // prod은 나중에
       ignoreEnvFile: process.env.NODE_ENV === 'prod', // production 환경일 땐 환경변수 파일 사용 X 다른 방법으로 얻는다.
       // validationSchema // 환경변수의 유효성 검증 (다음 강의)
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      // 안먹힌다!!!!!!
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      // host: 'localhost',
-      // port: 5000,
-      // username: 'daheeahn',
-      // password: '12345',
-      // database: 'nuber-eats',
       synchronize: true, // 데이터베이스를 내 모듈의 현재 상태로 마이그레이션한다는 뜻
       logging: true, // 데이터베이스에서 무슨 일이 일어나는지 콘솔에 표시
     }),
