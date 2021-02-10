c42fd9b855b7b44c96df6861f5ea649d23f04efc
daheeahn <deg9810@gmail.com>
2021-02-11 01:29:29 +0900

    # 3.2 Injecting The Repository (07:44)

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,21 +1,23 @@
 import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { RestaurantService } from './restaurant.service';
 // import { Query } from '@nestjs/common'; // 이거 아님!!!!!!!!!!!!1
 
 @Resolver((of) => Restaurant) // 꼭 이렇게 안해도 됨. 그냥 이름 붙여주는거. 직관성 높아짐.
 export class RestaurantResolver {
+  constructor(private readonly restaurantService: RestaurantService) {}
+
   // Query는 1번째 argument로 'function'이 필요하다. () => Boolean = reutnrs => Boolean
-  @Query((returns) => Boolean) // => Boolean is for graphql (required)
-  isPizzaGood(): Boolean {
-    // : Boolean is for typescript (optional)
-    return true;
-  }
+  // @Query((returns) => Boolean) // => Boolean is for graphql (required)
+  // isPizzaGood(): boolean {
+  //   // : Boolean is for typescript (optional)
+  //   return true;
+  // }
 
   @Query((returns) => [Restaurant])
-  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    console.log('veganOnly', veganOnly);
-    return [];
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantService.getAll();
   }
 
   @Mutation((returns) => Boolean)
