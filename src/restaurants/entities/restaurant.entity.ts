c56caec3bc9180355671a82e8b4292793a40ad33
daheeahn <deg9810@gmail.com>
2021-02-11 14:58:17 +0900

    # 3.6 Optional Types and Columns (08:42)

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,5 +1,6 @@
 import { ObjectType, Field, InputType } from '@nestjs/graphql';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { IsString, IsBoolean, Length, IsOptional } from 'class-validator';
 
 // entity에서 InputType 안써주고 이렇게 할 수도 있는데 추천은 X
 // @InputType({ isAbstract: true }) // 스키마에 포함되지 않길 원한다는 뜻. 이걸 어디선가 복사해서 쓴다는 뜻. 직접 쓰는게 아니라 어디로 확장시킨다는 뜻.
@@ -12,23 +13,30 @@ export class Restaurant {
   @Field((type) => Number)
   id: number;
 
-  @Field((type) => String)
-  @Column()
+  @Field((type) => String) // for graphql
+  @Column() // for db
+  @IsString() // for dto
+  @Length(5) // for dto
   name: string;
 
-  @Field((type) => Boolean)
-  @Column()
+  @Field((type) => Boolean, { nullable: true }) // for graphql
+  @Column({ default: true }) // for db
+  @IsOptional() // for dto
+  @IsBoolean() // for dto
   isVegan: boolean;
 
-  @Field((type) => String)
-  @Column()
+  @Field((type) => String, { defaultValue: '강남' })
+  @Column() // 여기에 default: 안써도 되긴 하네~ 위처럼 Column에만 default: true를 해줘도 되고(graphql에는 nullable), 여기처럼 graphql에만 해줘도 되고 상관없나보다
+  @IsString()
   address: string;
 
   @Field((type) => String)
   @Column()
+  @IsString()
   ownerName: string;
 
   @Field((type) => String)
   @Column()
+  @IsString()
   categoryName: string;
 }
