24a91d093a03434ae91a93e76e4485839f5cd638
daheeahn <deg9810@gmail.com>
2021-02-11 18:53:08 +0900

    4.2 User Resolver and Service (05:20)

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
 import { Restaurant } from './restaurants/entities/restaurant.entity';
 import { UsersModule } from './users/users.module';
 import { CommonModule } from './common/common.module';
-import { User } from './users/entities/user.entity';
+import { User } from './users/entities/users.entity';
 
 // console.log(Joi); // 그냥 import Joi하면 undefined가 찍힘
 
