2cabd62cc9de2f1acebcf5509fec905d72bf38f7
daheeahn <deg9810@gmail.com>
2021-02-11 20:05:27 +0900

    # 4.7 Hashing Passwords (09:02)

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column } from 'typeorm';
+import { Entity, Column, BeforeInsert } from 'typeorm';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import {
   ObjectType,
@@ -6,6 +6,8 @@ import {
   Field,
   registerEnumType,
 } from '@nestjs/graphql';
+import * as bcrypt from 'bcrypt';
+import { InternalServerErrorException } from '@nestjs/common';
 
 enum UserRole {
   Owner,
@@ -29,4 +31,18 @@ export class User extends CoreEntity {
   @Column({ type: 'enum', enum: UserRole })
   @Field((type) => UserRole)
   role: UserRole;
+
+  @BeforeInsert()
+  async hashPassword(): Promise<void> {
+    // this.users.save 전에 create했을 때 이미 우리는 instance를 가지고 있다. (이 때 생성된다.)
+    // create는 단지 entity를 만들 뿐이야. 이 entity를 save하기 전에 hashPassword가 실행되는거고. BeforeInsert
+    // this.password에 접근할 수 있다. 그래서 해시!
+    try {
+      // throw new Error('oppp'); // for test
+      this.password = await bcrypt.hash(this.password, 10);
+    } catch (error) {
+      console.log('hash password', error);
+      throw new InternalServerErrorException();
+    }
+  }
 }
