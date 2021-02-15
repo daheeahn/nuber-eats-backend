f1b07bc5fdce71bd59d532ef209dbb35dec30722
daheeahn <deg9810@gmail.com>
2021-02-15 13:29:35 +0900

    안쓰는 코드 삭제

--- src/restaurants/restaurant.service.ts
+++ src/restaurants/restaurant.service.ts
@@ -3,7 +3,6 @@ import { Restaurant } from './entities/restaurant.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
-import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 
 @Injectable()
 export class RestaurantService {
@@ -13,10 +12,6 @@ export class RestaurantService {
     private readonly restaurants: Repository<Restaurant>,
   ) {}
 
-  getAll(): Promise<Restaurant[]> {
-    return this.restaurants.find();
-  }
-
   createRestaurant(
     createRestaurantDto: CreateRestaurantDto,
   ): Promise<Restaurant> {
@@ -25,15 +20,5 @@ export class RestaurantService {
     // save return Promise<Entity> (db 반영 O)
     const newRestaurant = this.restaurants.create(createRestaurantDto); // js에서만 존재하고 실제 db에 저장되진 않는다.
     return this.restaurants.save(newRestaurant);
-  }
-
-  updateRestaurant({ id, data }: UpdateRestaurantDto) {
-    // update: Does not check if entity exist in the database.
-    // => id가 존재하지 않아도 진행될 것이다.
-    console.log('======== data');
-    console.log(data);
-    console.log('-------- ...data');
-    console.log({ ...data });
-    return this.restaurants.update(id, { ...data });
   }
 }
